3855eca024f10215f73e8f04baa05fa8a820c7e4
maru pereira <martixana@gmail.com>
2024-02-28 14:04:59 +0000

    updates to  exercises

---
+++ Fichas_java/src/Ficha_01/Ex_09.java
@@ -0,0 +1,18 @@
+package Ficha_01;
+
+import java.util.Scanner;
+
+/*Foi contactado uma empresa XPTO,Lda. para a elaboração de um programa que permita calcular o vencimento de um colaborador,
+bem como os descontos e impostos decorrentes desse processo salarial. Conceitos:
+
+a - vencimento base 40 euros por dia
+b - subsídio de alimentação: 5 euros por dia
+c - */
+public class Ex_09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //declarar variáveis
+
+    }
+}
