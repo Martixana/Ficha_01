c97fcf3bf8ba28dfa84df68794be551d46b7f571
maru <martixana@gmail.com>
2024-03-01 11:57:25 +0000

    Update Ex_09.java

--- Fichas_java/src/Ficha_01/Ex_09.java
+++ Fichas_java/src/Ficha_01/Ex_09.java
@@ -7,7 +7,16 @@ bem como os descontos e impostos decorrentes desse processo salarial. Conceitos:
 
 a - vencimento base 40 euros por dia
 b - subsídio de alimentação: 5 euros por dia
-c - */
+c - Retenção de IRS: 10%
+d - Seg. Social 11%  encargo do funcionário e 23.75% a cargo da entidade patronal.
+
+Deve ser solicitado ao utilizador o código do funcionário e o número de dias que ele trabalhou nesse mês. Deverá depois apresentar:
+  a- o valor líquido a receber fruto do seu vencimento
+  b - total subsídio de alimentação
+  c - valor da retenção de IRS a entregar ao estado
+  d - valor total a entregar à S.S. (ambos os encargos)
+  e - valor líquido a receber pelo funcionário
+  */
 public class Ex_09 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
