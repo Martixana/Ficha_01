083fba473bbc5718df59f3afc03d9e3ad042ffc7
maru <martixana@gmail.com>
2024-03-01 12:46:46 +0000

    Update Ex_09.java

--- Fichas_java/src/Ficha_01/Ex_09.java
+++ Fichas_java/src/Ficha_01/Ex_09.java
@@ -2,7 +2,7 @@ package Ficha_01;
 
 import java.util.Scanner;
 
-/*Foi contactado uma empresa XPTO,Lda. para a elaboração de um programa que permita calcular o vencimento de um colaborador,
+/*Foi contactado por uma empresa XPTO,Lda. para a elaboração de um programa que permita calcular o vencimento de um colaborador,
 bem como os descontos e impostos decorrentes desse processo salarial. Conceitos:
 
 a - vencimento base 40 euros por dia
@@ -22,6 +22,40 @@ public class Ex_09 {
         Scanner input = new Scanner(System.in);
 
         //declarar variáveis
+        int vencimento, subsidioA, cod_funcionario, dias_trabalhados;
+        double irs_10, seg_social_func, seg_social_patronal;
+
+        //guardar valores int
+        vencimento = 40;
+        subsidioA = 5;
+
+        //guardar valores double
+        irs_10 = 0.10;
+        seg_social_func = 0.11;
+        seg_social_patronal = 0.24;
+
+        //Pedir código de funcionário
+        System.out.println ("Qual é o seu código de funcionário");
+        //guardar valor
+        cod_funcionario= input.nextInt();
+
+        //número de dias que trabalhou nesse mês
+        System.out.println ("Quantos dias trabalhou no mês passado?");
+        //guardar valor
+        dias_trabalhados= input.nextInt();
+
+        //a- o valor líquido a receber fruto do seu vencimento
+
+
+
+        // b - total subsídio de alimentação
+        // c - valor da retenção de IRS a entregar ao estado
+        // d - valor total a entregar à S.S. (ambos os encargos)
+        // e - valor líquido a receber pelo funcionário
+
+
+
+
 
     }
 }
